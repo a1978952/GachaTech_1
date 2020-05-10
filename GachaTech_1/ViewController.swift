b01fbca05f1cbc5728cd1bfbeeee847552a2f187
Ruki Fujiwara <rukiwillgotoksf@gmail.com>
2020-05-10 17:20:32 +0900

    oaoaoaoaoaaoa

--- GachaTech_1/ViewController.swift
+++ GachaTech_1/ViewController.swift
@@ -14,10 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    @IBAction func gacha(){
-        self.performSegue(withIdentifier:"result",sender: nil )
-        
-    }
-
+    
+    
 }
 
